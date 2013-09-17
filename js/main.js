a7bb9e9c09c025fe3be37d86240575f3285e9ee2
Lev Kanter <levkanter@gmail.com>
2013-09-17 17:41:16 -0400

    fix indentation

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
 require.config({
-	paths: {
-		jquery: '../lib/jquery/jquery.min',
-		underscore: '../lib/underscore/underscore-min'
-	},
-	shim: {
-		underscore: {
-			exports: '_'
-		}
-	},
-	packages: [
-		{
-			name: 'larch',
-			main: 'larch',
-			location: 'larch'
-		}
-	]
+    paths: {
+        jquery: '../lib/jquery/jquery.min',
+        underscore: '../lib/underscore/underscore-min'
+    },
+    shim: {
+        underscore: {
+            exports: '_'
+        }
+    },
+    packages: [
+        {
+            name: 'larch',
+            main: 'larch',
+            location: 'larch'
+        }
+    ]
 });
 
 require(['jquery', 'underscore', 'larch', 'larch/validation'], function($, _, larch, validation) {
-	$(function() {
-		console.dir(larch);
-		console.dir(validation);
-	});
+    $(function() {
+        console.dir(larch);
+        console.dir(validation);
+    });
 });
